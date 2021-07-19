dff98d8da353b71acda800b7d80c3fe5f684f45f
khushbookedia <73025705+khushbookedia@users.noreply.github.com>
2021-07-19 18:32:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var images = [
+  "https://s3-projectsassets.whjr.online/Monkey_02.png",
+  "https://s3-projectsassets.whjr.online/Monkey_03.png",
+  "https://s3-projectsassets.whjr.online/Monkey_04.png",
+  "https://s3-projectsassets.whjr.online/Monkey_05.png",
+  "https://s3-projectsassets.whjr.online/Monkey_06.png",
+  "https://s3-projectsassets.whjr.online/Monkey_07.png",
+  "https://s3-projectsassets.whjr.online/Monkey_08.png",
+  "https://s3-projectsassets.whjr.online/Monkey_09.png",
+  "https://s3-projectsassets.whjr.online/Monkey_10.png"
+];
+
+var i = 0
+
+function moving() {
+    document.getElementById("album").src = images[i];
+
+    if (i == 8) {
+        i = 0
+    } else {
+        i++
+    }
+}
